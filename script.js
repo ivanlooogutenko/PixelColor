6e6ce64420dc747ff83bb34b0b2fd237c27749eb
Иван <137431511+ivanlooogutenko@users.noreply.github.com>
2025-09-11 17:14:31 +0300

    v7

--- script.js
+++ script.js
@@ -3,21 +3,29 @@ const artData = {
     width: 10,
     height: 10,
     palette: [
-        { id: 1, color: "#4CAF50" }, // Зеленый
-        { id: 2, color: "#FFC107" }, // Желтый
-        { id: 3, color: "#2196F3" }  // Синий
+        { id: 1, color: "#87CEEB" }, // Light blue
+        { id: 2, color: "#FFD700" }, // Yellow
+        { id: 3, color: "#372e2aff" }, // Black
     ],
     pixels: [
-        { x: 4, y: 0, paletteId: 2 }, { x: 5, y: 0, paletteId: 2 },
-        { x: 3, y: 1, paletteId: 2 }, { x: 6, y: 1, paletteId: 2 },
-        { x: 2, y: 2, paletteId: 2 }, { x: 7, y: 2, paletteId: 2 },
-        { x: 2, y: 3, paletteId: 2 }, { x: 7, y: 3, paletteId: 2 },
-        { x: 0, y: 4, paletteId: 1 }, { x: 1, y: 4, paletteId: 1 }, { x: 8, y: 4, paletteId: 1 }, { x: 9, y: 4, paletteId: 1 },
-        { x: 0, y: 5, paletteId: 1 }, { x: 2, y: 5, paletteId: 3 }, { x: 3, y: 5, paletteId: 3 }, { x: 4, y: 5, paletteId: 3 }, { x: 5, y: 5, paletteId: 3 }, { x: 6, y: 5, paletteId: 3 }, { x: 7, y: 5, paletteId: 3 }, { x: 9, y: 5, paletteId: 1 },
-        { x: 0, y: 6, paletteId: 1 }, { x: 2, y: 6, paletteId: 3 }, { x: 7, y: 6, paletteId: 3 }, { x: 9, y: 6, paletteId: 1 },
-        { x: 0, y: 7, paletteId: 1 }, { x: 2, y: 7, paletteId: 3 }, { x: 7, y: 7, paletteId: 3 }, { x: 9, y: 7, paletteId: 1 },
-        { x: 0, y: 8, paletteId: 1 }, { x: 3, y: 8, paletteId: 1 }, { x: 4, y: 8, paletteId: 1 }, { x: 5, y: 8, paletteId: 1 }, { x: 6, y: 8, paletteId: 1 }, { x: 9, y: 8, paletteId: 1 },
-        { x: 0, y: 9, paletteId: 1 }, { x: 9, y: 9, paletteId: 1 }
+        // Light Blue (id: 1)
+        { x: 1, y: 5, paletteId: 1 }, { x: 1, y: 6, paletteId: 1 },
+        { x: 2, y: 4, paletteId: 1 }, { x: 2, y: 5, paletteId: 1 }, { x: 2, y: 6, paletteId: 1 },
+        { x: 7, y: 4, paletteId: 1 }, { x: 7, y: 5, paletteId: 1 }, { x: 7, y: 6, paletteId: 1 },
+        { x: 8, y: 5, paletteId: 1 }, { x: 8, y: 6, paletteId: 1 },
+
+        // Yellow (id: 2)
+        { x: 4, y: 2, paletteId: 2 }, { x: 5, y: 2, paletteId: 2 },
+        { x: 3, y: 3, paletteId: 2 }, { x: 4, y: 3, paletteId: 2 }, { x: 5, y: 3, paletteId: 2 }, { x: 6, y: 3, paletteId: 2 },
+        { x: 3, y: 5, paletteId: 2 }, { x: 4, y: 5, paletteId: 2 }, { x: 5, y: 5, paletteId: 2 }, { x: 6, y: 5, paletteId: 2 },
+        { x: 3, y: 7, paletteId: 2 }, { x: 4, y: 7, paletteId: 2 }, { x: 5, y: 7, paletteId: 2 }, { x: 6, y: 7, paletteId: 2 },
+
+        // Black (id: 3)
+        { x: 3, y: 2, paletteId: 3 }, { x: 6, y: 2, paletteId: 3 },
+        { x: 3, y: 4, paletteId: 3 }, { x: 4, y: 4, paletteId: 3 }, { x: 5, y: 4, paletteId: 3 }, { x: 6, y: 4, paletteId: 3 },
+        { x: 3, y: 6, paletteId: 3 }, { x: 4, y: 6, paletteId: 3 }, { x: 5, y: 6, paletteId: 3 }, { x: 6, y: 6, paletteId: 3 },
+        { x: 4, y: 8, paletteId: 3 }, { x: 5, y: 8, paletteId: 3 },
+
     ]
 };
 
